2cd5a06a42a9b4874278be0997325a9adf0e25ae
Ilgam Shakirov <shilgam@gmail.com>
2022-03-30 12:31:07 +0300

    Autofix codestyle issues

--- src/rxjs.example.test.js
+++ src/rxjs.example.test.js
@@ -1,7 +1,7 @@
 import { of } from 'rxjs';
 
-test('the observable emits hello', done => {
-  of('hello').subscribe( data => {
+test('the observable emits hello', (done) => {
+  of('hello').subscribe((data) => {
     expect(data).toBe('hello');
     done();
   });
--- src/sum.js
+++ src/sum.js
@@ -1,6 +1,6 @@
 // https://jestjs.io/ru/docs/getting-started
 
 function sum(a, b) {
-    return a + b;
-  }
-  module.exports = sum;
+  return a + b;
+}
+module.exports = sum;
